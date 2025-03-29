a919882c3368ef60b08e52347b5d02f05f8e7af6
Jok <66626821+JOKleinGe1@users.noreply.github.com>
2025-03-29 09:46:51 +0100

    Update test.c
    delete uart

--- test.c
+++ test.c
@@ -2,32 +2,12 @@
 #define   LEDRADDRESS   (*(volatile char *)  0x8000)
 #define   SWADDRESS   (*(volatile char *)  0x8004)
 #define   HEXADDRESS   (*(volatile unsigned int *)  0x8010)
-#define   UARTSRADDRESS   (*(volatile char *)  0x8068)
-#define   UARTCRADDRESS   (*(volatile char *)  0x8064)
-#define   TXDATAADDRESS   (*(volatile char *)  0x8060)
-#define   RXDATAADDRESS   (*(volatile char *)  0x806C)
-
-void tx_write (unsigned c){
-  while(UARTSRADDRESS&0x01);
-  TXDATAADDRESS = c;
-}
-
-char rx_read (void){
-  char c;
-  while((UARTSRADDRESS&0x02) != 0x02);
-  c = RXDATAADDRESS;
-}
 
 int main (void){
   int i = 0;
-  char c;
-  UARTCRADDRESS = 0x03; //enable RX and TX
   while (1){
-    c = rx_read();
-    HEXADDRESS = c; 
-    tx_write (c);
-    LEDRADDRESS = i++;
+    HEXADDRESS = SWADDRESS; 
+    LEDRADDRESS = (i++)>>10;
   }
   return 0;  
 }
-
